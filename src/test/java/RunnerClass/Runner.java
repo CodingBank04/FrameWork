a78a303896d31b2a2837f8156eff46a897b3d9c3
GURU-JAVA <54605558+beehive04@users.noreply.github.com>
2019-11-05 19:33:01 -0700

    2 st commit

--- src/test/java/RunnerClass/Runner.java
+++ src/test/java/RunnerClass/Runner.java
@@ -1,10 +1,1 @@
-package RunnerClass;
-import org.junit.runner.RunWith;
-import cucumber.api.CucumberOptions;
-import cucumber.api.junit.Cucumber;
-
-@RunWith(Cucumber.class)
-@CucumberOptions(features="src/test/java/FeatureFiles",glue= "Stepdef")
-public class Runner {
-
-}
+package RunnerClass;import org.junit.runner.RunWith;import cucumber.api.CucumberOptions;import cucumber.api.junit.Cucumber;@RunWith(Cucumber.class)@CucumberOptions(features="src/test/java/FeatureFiles",glue= "Stepdef")public class Runner {}
--- src/test/java/Stepdef/StepDef.java
+++ src/test/java/Stepdef/StepDef.java
@@ -1,6 +1,8 @@
 package Stepdef;
 
+import cucumber.api.DataTable;
 import cucumber.api.PendingException;
+import cucumber.api.java.en.And;
 import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
@@ -8,23 +10,63 @@ import cucumber.api.java.en.Given;
 import cucumber.api.java.en.When;
 import cucumber.api.java.en.Then;
 
-    @RunWith(Cucumber.class)
+import java.util.List;
+
+@RunWith(Cucumber.class)
     public class StepDef {
 
-            @Given("^User will navigate to the website$")
+
+    @Given("^User will navigate to the website$")
             public void user_will_navigate_to_the_website() throws Throwable {
                 System.out.println("User will navigate to the website");
             }
 
-            @When("^User login into application with username and password$")
-            public void user_login_into_application_with_username_and_password() throws Throwable {
-                System.out.println("User login into application with username and password");
-            }
+        @When("^User login into application with \"([^\"]*)\" and \"([^\"]*)\"$")
+        public void user_login_into_application_with_and(String arg1, String arg2) throws Throwable {
+            // Write code here that turns the phrase above into concrete actions
+            System.out.println(arg1);
+            System.out.println(arg2);
+        }
 
-            @Then("^DashBoard will appear$")
-            public void dashboard_will_appear() throws Throwable {
-                System.out.println("DashBoard will appear");
-            }
+
+
+        @Then("^DashBoard will appear with \"([^\"]*)\"$")
+        public void dashboard_will_appear_with_something(String strArg1) throws Throwable {
+        }
+
+
+
+        @And("^user will see something$")
+        public void user_will_see_something() throws Throwable {
+            System.out.println("user will see something");
+        }
+        @When("^User sign into application with following information$")
+        public void user_sign_into_application_with_following_information(DataTable data) throws Throwable {
+
+            List<List<String>> obj=data.raw();
+            System.out.println(obj.get(0).get(0));
+            System.out.println(obj.get(0).get(1));
+            System.out.println(obj.get(0).get(2));
+            System.out.println(obj.get(0).get(3));
+            System.out.println(obj.get(0).get(4));
+            System.out.println(obj.get(0).get(5));
+            System.out.println(obj.get(0).get(6));
+
 
         }
+    @When("^User login into  with \"([^\"]*)\" and \"([^\"]*)\"$")
+    public void user_login_into_with_and(String arg1, String arg2) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+    }
+    @Then("^DashBoard will appear with$")
+    public void dashboard_will_appear_with() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+    }
+
+
+}
+
+
+
+
 
